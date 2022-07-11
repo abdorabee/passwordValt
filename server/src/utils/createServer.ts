d8ad350650b514d90cca5cb05b2b173542facb23
Abdo <abdorabee1134@gmail.com>
2022-07-11 15:34:07 +0200

    added the user and vault module

--- server/src/utils/createServer.ts
+++ server/src/utils/createServer.ts
@@ -1,10 +1,36 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { CORS_ORIGIN } from "../constants";
+import jwt from "@fastify/jwt";
+import fs from "fs";
+import path from "path";
 
-function createServer(){
- 
-    const app = fastify()
+function createServer() {
+  const app = fastify();
 
-    return app ;
+  app.register(cors, {
+    origin: CORS_ORIGIN,
+    credentials: true,
+  });
+
+  app.register(jwt, {
+    secret: {
+      private: fs.readFileSync(
+        `${(path.join(process.cwd()), "certs")}/private.key`
+      ),
+      public: fs.readFileSync(
+        `${(path.join(process.cwd()), "certs")}/public.key`
+      ),
+    },
+    sign: {
+      algorithm: "RS256",
+    },
+    cookie: {
+      cookieName: "token",
+      signed: false,
+    },
+  });
+  return app;
 }
 
 export default createServer;
